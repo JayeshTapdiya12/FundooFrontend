da3063d70b06b7ec9b877f0e2e03d7f03546f16e
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2024-08-13 14:20:48 +0530

    [jayesh]edit:added login page to the fundoofrontend

--- fundoonotesfrontend/src/App.js
+++ fundoonotesfrontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import SignUp from './Pages/SignUp';
+import Login from './Pages/Login';
 
 function App() {
   return (
     <div className="App">
 
       <SignUp />
+      <Login />
     </div>
   );
 }
--- fundoonotesfrontend/src/Pages/Login.jsx
+++ fundoonotesfrontend/src/Pages/Login.jsx
@@ -1,20 +1,60 @@
 import React, { useState } from 'react'
-import Button from '@mui/material/Button';
+import { Container, TextField, Button, Typography, Link, Box } from '@mui/material';
+import '../Style/login.css'
 
-function Login(props) {
-    const { name, lchange } = props
-    // const [lname, setLname]=useState()   
-    const calling = () => {
-        lchange("hello")
-    }
+function Login() {
+
 
     return (
         <>
-            <h1>{name}</h1>
+            <form action="">
+                <Container maxWidth="xs">
+                    <Box textAlign="center" marginBottom={2}>
+                        <Typography variant="h5" gutterBottom>
+                            Login
+                        </Typography>
+                        <Typography variant="subtitle1">
+                            Use your Google Account
+                        </Typography>
+                    </Box>
 
-            <Button variant="contained" onClick={calling}>Last Name</Button>
-            {/* {name === "jayesh" ? <p>hi jayesh</p> : <p>{lname}</p>} */}
+                    <TextField
+                        type='email'
+                        label="Email or phone"
+                        variant="outlined"
+                        margin="normal"
+                        fullWidth
+                        required
+                    />
 
+                    <TextField
+
+                        label="Password"
+                        type="password"
+                        variant="outlined"
+                        margin="normal"
+                        fullWidth
+                        required
+                    />
+
+                    <Box textAlign="right" marginBottom={2}>
+                        <Link href="#" variant="body2">
+                            Forgot Password?
+                        </Link>
+                    </Box>
+
+
+                    <Button type="submit" variant="contained" color="primary">
+                        login
+                    </Button>
+
+                    <Box textAlign="center" marginTop={2}>
+                        <Link href="#" variant="body2">
+                            Create account
+                        </Link>
+                    </Box>
+                </Container>
+            </form>
         </>
     )
 }
