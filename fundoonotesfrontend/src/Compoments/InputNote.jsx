8ab031748486bb39d496e623dde08a1feac48f32
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2024-08-23 15:05:00 +0530

    [jayesh]edit:added new functionalaty for the to create new notes in the users

--- fundoonotesfrontend/src/Compoments/InputNote.jsx
+++ fundoonotesfrontend/src/Compoments/InputNote.jsx
@@ -2,27 +2,61 @@ import React, { useState } from 'react'
 import '../Style/search.css'
 import IconBaar from './IconBaar';
 
+import { createNote } from '../Services/NoteService';
 
 
 
-
-export default function InputNote() {
+export default function InputNote({ setNoteCreated }) {
     const [isExpanded, setIsExpanded] = useState(false);
+
+
+    const [data, setData] = useState({
+        title: "",
+        description: ""
+
+    })
 
     const handleOpen = () => {
         setIsExpanded(true)
     }
 
+
+    const send = async () => {
+
+        try {
+            if (data.title !== "" || data.description !== "") {
+                const res = await createNote(data);
+                console.log(res);
+                setNoteCreated(true)
+            }
+        } catch (error) {
+            console.error("Error creating note:", error);
+        }
+    }
+
     const handleToggle = () => {
-        setIsExpanded(false);
+
+        if (data.title === "" && data.description === "") {
+            setIsExpanded(false);
+        } else {
+            send();
+            setIsExpanded(false);
+            setData({
+                title: "",
+                description: ""
+            })
+        }
+
     };
+
+
 
     return (
         <>
             <div className={`note-input ${isExpanded ? 'expanded' : ''}`}>
                 {isExpanded && (
                     <div className="note-form">
-                        <input type="text" placeholder="Title" className="title-input" />
+                        <input type="text" placeholder="Title" className="title-input" name="title" value={data.title} onChange={(e) => setData({ ...data, [e.target.name]: e.target.value })} />
                     </div>
                 )}
                 <div className="note-header" onClick={handleOpen}>
@@ -30,6 +64,7 @@ export default function InputNote() {
                         type="text"
                         placeholder="Take a note..."
                         className={`note-input-field ${isExpanded ? 'expanded-field' : ''}`}
+                        name="description" value={data.description} onChange={(e) => setData({ ...data, [e.target.name]: e.target.value })}
                     />
                 </div>
                 {isExpanded && (
