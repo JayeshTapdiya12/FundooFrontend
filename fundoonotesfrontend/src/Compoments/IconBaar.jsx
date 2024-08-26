a0988f551f65f65d253efe27398d19bb2b17d15a
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2024-08-26 18:43:32 +0530

    [jayesh]refactor: make new function to make the iconbar with spefici icon on the particular compoments

--- fundoonotesfrontend/src/Compoments/IconBaar.jsx
+++ fundoonotesfrontend/src/Compoments/IconBaar.jsx
@@ -1,85 +1,193 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
 import NotificationsIcon from '@mui/icons-material/NotificationsOutlined';
 import GroupAddIcon from '@mui/icons-material/GroupAddOutlined';
 import LightbulbIcon from '@mui/icons-material/LightbulbOutlined';
-// import ImageIcon from '@mui/icons-material/ImageOutlined';
-import CalendarTodayIcon from '@mui/icons-material/ArchiveOutlined';
-import MoreVertIcon from '@mui/icons-material/MoreVertOutlined';
+import ArchiveOutlined from '@mui/icons-material/ArchiveOutlined';
 import ArrowBackIcon from '@mui/icons-material/ArrowBackOutlined';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForwardOutlined'
+import ArrowForwardIcon from '@mui/icons-material/ArrowForwardOutlined';
 import ColorLensOutlinedIcon from '@mui/icons-material/ColorLensOutlined';
-import '../Style/IconBar.css'
+import '../Style/IconBar.css';
 import ColorPicker from './ColorPicker';
+import { useParams } from 'react-router-dom';
+import { archiveNote } from '../Services/NoteService';
+import BasicMenu from './Menu';
 
-export default function IconBaar() {
-
+export default function IconBaar({ setIcon, tabV }) {
     const [isExpanded, setIsExpanded] = useState(false);
+    const [more, setMore] = useState(false);
 
+    const { id } = useParams();
+    console.log('id in icon barr========>', id)
 
     const handleToggle = () => {
         setIsExpanded(!isExpanded);
     };
 
-    return (
-        <>
-            <div className="icon-baar">
-                <div className="icon-group">
-                    <Tooltip title="Notifications" arrow>
-                        <IconButton>
-                            <NotificationsIcon />
-                        </IconButton>
-                    </Tooltip>
-                    <Tooltip title="Add Group" arrow>
-                        <IconButton>
-                            <GroupAddIcon />
-                        </IconButton>
-                    </Tooltip>
-                    <Tooltip title="Colors" arrow>
-                        <IconButton>
-                            <ColorLensOutlinedIcon onClick={handleToggle} />
-                            {isExpanded && (
-                                <div className="color-picker-container">
-                                    <ColorPicker />
-                                </div>)}
-                        </IconButton>
-                    </Tooltip>
-                    <Tooltip title="Ideas" arrow>
-                        <IconButton>
-                            <LightbulbIcon />
-                        </IconButton>
-                    </Tooltip>
+    const handleToggle2 = () => {
+        setMore(!more);
+    };
 
+    const [icons, setIcons] = useState(setIcon);
+    console.log("values of icon============>", icons, setIcon)
+    console.log("values of tabs ====>", tabV)
+
+    const archi = async () => {
+        if (id) {
+            console.log(id);
+            try {
+                const res = await archiveNote(id);
+                console.log('Note archived:', res);
+            } catch (error) {
+                console.error('Failed to archive note:', error);
+            }
+        } else {
+            console.error('No id found in params');
+        }
+    };
+
+    const renderIcons = () => {
+        switch (true) {
+            case icons === 1:
+                return (
+                    <>
+                        <Tooltip title="Notifications" arrow>
+                            <IconButton>
+                                <NotificationsIcon />
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip title="Add Group" arrow>
+                            <IconButton>
+                                <GroupAddIcon />
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip title="Colors" arrow>
+                            <IconButton onClick={handleToggle}>
+                                <ColorLensOutlinedIcon />
+                                {isExpanded && (
+                                    <div className="color-picker-container">
+                                        <ColorPicker />
+                                    </div>
+                                )}
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip title="Ideas" arrow>
+                            <IconButton>
+                                <LightbulbIcon />
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip title="Archive" arrow>
+                            <IconButton onClick={archi}>
+                                <ArchiveOutlined />
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip title="More Options" arrow>
+                            <IconButton onClick={handleToggle2}>
+                                <div className="div">
+                                    <BasicMenu />
+                                </div>
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip title="Undo" arrow>
+                            <IconButton>
+                                <ArrowBackIcon />
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip title="Redo" arrow>
+                            <IconButton>
+                                <ArrowForwardIcon />
+                            </IconButton>
+                        </Tooltip>
+                    </>
+                );
+            case icons === 2 && tabV === 1:
+                return (
+                    <>
+                        <Tooltip title="Notifications" arrow>
+                            <IconButton>
+                                <NotificationsIcon />
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip title="Add Group" arrow>
+                            <IconButton>
+                                <GroupAddIcon />
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip title="Colors" arrow>
+                            <IconButton onClick={handleToggle}>
+                                <ColorLensOutlinedIcon />
+                                {isExpanded && (
+                                    <div className="color-picker-container">
+                                        <ColorPicker />
+                                    </div>
+                                )}
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip title="Ideas" arrow>
+                            <IconButton>
+                                <LightbulbIcon />
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip title="Archive" arrow>
+                            <IconButton onClick={archi}>
+                                <ArchiveOutlined />
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip title="More Options" arrow>
+                            <IconButton onClick={handleToggle2}>
+                                <div className="div">
+                                    <BasicMenu />
+                                </div>
+                            </IconButton>
+                        </Tooltip>
+                    </>
+                );
+            // archive
+            case tabV === 4:
+                return (<>
                     <Tooltip title="Archive" arrow>
-                        <IconButton>
-                            <CalendarTodayIcon />
+                        <IconButton onClick={archi}>
+                            <ArchiveOutlined />
                         </IconButton>
                     </Tooltip>
-
                     <Tooltip title="More Options" arrow>
-                        <IconButton>
-                            <MoreVertIcon />
+                        <IconButton onClick={handleToggle2}>
+                            <div className="div">
+                                <BasicMenu />
+                            </div>
                         </IconButton>
                     </Tooltip>
-                    <Tooltip title="Undo" arrow>
-                        <IconButton>
-                            <ArrowBackIcon />
+                </>
+                );
+            // delte
+            case tabV === 5:
+                return (<>
+                    <Tooltip title="Archive" arrow>
+                        <IconButton onClick={archi}>
+                            <ArchiveOutlined />
                         </IconButton>
                     </Tooltip>
-                    <Tooltip title="Redo" arrow>
-                        <IconButton>
-                            <ArrowForwardIcon />
+                    <Tooltip title="More Options" arrow>
+                        <IconButton onClick={handleToggle2}>
+                            <div className="div">
+                                <BasicMenu />
+                            </div>
                         </IconButton>
                     </Tooltip>
-                    {/* <br /> */}
+                </>
+                );
 
+            default:
+                return null;
+        }
+    };
 
-                </div>
-
+    return (
+        <div className="icon-baar">
+            <div className="icon-group">
+                {renderIcons()}
             </div>
-
-        </>
-    )
+        </div>
+    );
 }
