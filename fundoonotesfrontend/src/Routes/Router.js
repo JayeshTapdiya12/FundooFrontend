e8ec1fb39da551e9ca4334159b77c9ed743a7786
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2024-08-26 18:48:27 +0530

    [jayesh]edit:addede new routing for the single note edit

--- fundoonotesfrontend/src/Routes/Router.js
+++ fundoonotesfrontend/src/Routes/Router.js
@@ -5,6 +5,8 @@ import Login from '../Pages/Login'
 import Dashboard from '../Pages/Dashboard'
 import { AuthRoute } from './AuthRoute'
 import { ProtectedRoute } from './ProtectedRoute'
+import SingleNote from '../Compoments/SingleNote'
+import PopNote from '../Compoments/PopNote'
 export default function Router() {
     return (
         <>
@@ -13,11 +15,15 @@ export default function Router() {
 
                     <Route path='/' exact element={<AuthRoute> <SignUp /> </AuthRoute>} />
                     <Route path='/login' element={<AuthRoute> <Login /></AuthRoute>} />
-                    <Route path='/dashboard' element={<ProtectedRoute> <Dashboard /> </ProtectedRoute>} >
-                        <Route path='reminder' element={<ProtectedRoute></ProtectedRoute>} />
+                    <Route path='/dashboard/' element={<ProtectedRoute> <Dashboard /> </ProtectedRoute>} >
+                        {/* <Route path="/dashboard/note/:id" element={<ProtectedRoute><PopNote /></ProtectedRoute>} /> */}
+                        <Route path="/dashboard/note/:id" element={<ProtectedRoute><PopNote /></ProtectedRoute>} />
+                        {/* <Route path='reminder' element={<ProtectedRoute> </ProtectedRoute>} />
                         <Route path='archive' element={<ProtectedRoute></ProtectedRoute>} />
                         <Route path='editlables' element={<ProtectedRoute></ProtectedRoute>} />
-                        <Route path='trash' element={<ProtectedRoute></ProtectedRoute>} />
+                        <Route path='trash' element={<ProtectedRoute></ProtectedRoute>} /> */}
+
+
 
                     </Route>
                 </Routes>
